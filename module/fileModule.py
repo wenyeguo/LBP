8af7a02984fbddd33050afe0d7dc221c3ab23ebb
wenyeguo <wenyeguo@outlook.com>
2024-04-12 19:07:24 -0700

    update fileModule

--- module/fileModule.py
+++ module/fileModule.py
@@ -4,7 +4,7 @@ import pickle
 
 class File:
     def __init__(self, name):
-        self.name = name
+        self.filename = name
         self.data = None
 
     def get_data(self):
@@ -16,16 +16,25 @@ class File:
         self.write_data_to_file()
 
     def read_file_from_file(self):
-        with open(self.name, 'rb') as f:
+        with open(self.filename, 'rb') as f:
             self.data = pickle.load(f)
 
     def write_data_to_file(self):
-        with open(self.name, 'wb+') as f:
+        with open(self.filename, 'wb+') as f:
             pickle.dump(self.data, f)
 
     def store_data_into_csvFile(self, rowName, data):
-        with (open(self.name, 'w', newline='') as csv_file):
+        with (open(self.filename, 'w', newline='') as csv_file):
             writer = csv.writer(csv_file)
             writer.writerow(rowName)
             for key, value in data.items():
                 writer.writerow([key] + value)
+
+    def store_threshold_result_into_csv_file(self, data):
+        with open(self.filename, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['ths+', 'ths-', 'accuracy', 'recursion', 'precision', 'F1'])
+            for key, val in data.items():
+                t1, t2 = key.split(':')
+                accuracy, precision, recall, f1 = val[0], val[1], val[2], val[3]
+                writer.writerow([t1, t2, accuracy, precision, recall, f1])
