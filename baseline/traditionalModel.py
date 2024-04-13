29974409d2361979a57ecdf6562c4580a15d8ded
wenyeguo <wenyeguo@outlook.com>
2024-04-12 18:46:06 -0700

    Add Traditional Model

--- baseline/traditionalModel.py
+++ baseline/traditionalModel.py
@@ -1,31 +1,49 @@
-import csv
-import pickle
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, cross_val_score, cross_val_predict, KFold
+from sklearn.naive_bayes import GaussianNB
 
-df = pd.read_csv('./features/url_features.csv')
-urls, features, labels = df["URL"].values, df.drop(labels=['URL', 'Label'], axis=1), df["Label"].values.astype('int')
+K_FOLD = KFold(n_splits=5, shuffle=True, random_state=0)
 
-# X - feature, Y - labels
-X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.25)
 
-model = RandomForestClassifier(random_state=16)
-cv_scores = cross_val_score(model, features, labels, cv=5)
-predictions = cross_val_predict(model, features, labels, cv=5, method='predict_proba')
-url_benign_probability = {}
-for i in range(len(urls)):
-    url_benign_probability[urls[i]] = round(predictions[i][0], 4)
+def get_metric(model, filename):
+    """train model"""
+    features_df = pd.read_csv(filename)
+    features = features_df.drop(['url', 'type', 'label'], axis=1) 
+    labels = features_df['label']
 
-# store probability to csv file
-print(url_benign_probability)
+    model_name = type(model).__name__
+    precisions = cross_val_score(model, features, labels, cv=K_FOLD, scoring='precision')
+    recalls = cross_val_score(model, features, labels, cv=K_FOLD, scoring='recall')
+    f1scores = cross_val_score(model, features, labels, cv=K_FOLD, scoring='f1')
+    accuracy = cross_val_score(model, features, labels, cv=K_FOLD, scoring='accuracy')
+    print(f'{model_name}:')
+    print(
+        f"Precision, Recall, F1 Score: {round(precisions.mean(), 4)}, {round(recalls.mean(), 4)}, {round(f1scores.mean(), 4)}")
+    print("Accuracy: ", round(accuracy.mean(), 4))
 
-with open('./probability/pickle/benign_probability_RandomForest.pickle', 'wb') as f:
-    pickle.dump(url_benign_probability, f)
-probaFile = './probability/csv/benign_probability_RandomForest.csv'
-with open(probaFile, 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(['URL', 'Probability (benign)'])
-    for url, proba in url_benign_probability.items():
-        row = [url, proba]
-        writer.writerow(row)
+
+def store_probability(model, filename):
+    """store probability into csv file"""
+    features_df = pd.read_csv(filename)
+    features = features_df.drop(['url', 'label'], axis=1)
+    urls = features_df['url']
+    labels = features_df['label']
+    predictions = cross_val_predict(model, features, labels, cv=K_FOLD, method='predict_proba')
+    url_probability = []
+    for i in range(len(urls)):
+        url_probability.append([urls[i], labels[i], round(predictions[i][0], 4), round(predictions[i][1], 4)])
+
+    model_name = type(model).__name__
+    columns = ['url', 'label', 'Probability (benign)', 'Probability (malicious)']
+    probability_df = pd.DataFrame(url_probability, columns=columns)
+    probability_df.to_csv(f"probability/benign_probability_{model_name}.csv", index=False, mode='w')
+
+
+models = [RandomForestClassifier(random_state=0), LogisticRegression(random_state=0), GaussianNB()]
+for model in models:
+    # change filename according resource
+    filename = f"features/normalized_features.csv" 
+    get_metric(model, filename)
+    # store_probability(model, filename)
