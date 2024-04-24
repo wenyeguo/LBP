647dd0e58a54e02a6cf85daf0ec3cde26566ad4d
wenyeguo <wenyeguo@outlook.com>
2024-04-24 11:47:17 -0700

    update workerModule.py

--- module/workerModule.py
+++ module/workerModule.py
@@ -1,5 +1,3 @@
-import multiprocessing
-
 from module.graphNodeModule import GraphNode
 from module.messageModule import Message
 from module.metricModule import Metric
@@ -8,6 +6,7 @@ from module.metricModule import Metric
 class Worker:
     def __init__(self, t):
         self.workerType = t
+        self.dataSuffix = None
         self.similarity = None
         self.urls = None
         self.graph = None
@@ -20,7 +19,7 @@ class Worker:
         self.add_test_probability = None
 
     def run(self, args):
-        (self.add_test_probability, self.edgePotentialType, self.classify_threshold, self.threshold1, self.threshold2,
+        (self.dataSuffix, self.add_test_probability, self.edgePotentialType, self.classify_threshold, self.threshold1, self.threshold2,
          self.graph, self.urls, self.similarity,
          self.data, self.processId) = args
         if self.workerType == 'cycle':
@@ -31,7 +30,7 @@ class Worker:
         return metrics.get_ROC_data()
 
     def worker_normal(self):
-        graph_node = GraphNode(self.graph)
+        graph_node = GraphNode(self.graph, self.dataSuffix)
         graph_node.set_classify_threshold(self.classify_threshold)
         graph_node.init_nodes(self.data, self.urls)
         if self.add_test_probability:
@@ -41,9 +40,9 @@ class Worker:
         self.messagePassing('normal', times)
 
     def worker_cycle(self):
-        # 1. delete cycles, passing message, predict labels
+        """delete cycles, passing message, predict labels"""
         # init graph nodes, add metadata (label, predict_label ...)
-        graph_node = GraphNode(self.graph)
+        graph_node = GraphNode(self.graph, self.dataSuffix)
         graph_node.set_classify_threshold(self.classify_threshold)
         graph_node.init_nodes(self.data, self.urls)
         if self.add_test_probability:
@@ -51,10 +50,10 @@ class Worker:
 
         self.graph = graph_node.remove_cycles()
         self.messagePassing('normal', -1)
-        #
+
         withoutPredictionNodesNums = self.withoutPredictionNodes()
         print(f'Process {self.processId}, after remove all cycles, not predicted nodes', withoutPredictionNodesNums)
-        #
+
         add_all_cycles_back = self.add_cycle_to_worker(graph_node)
         if not add_all_cycles_back:
             self.print_convergence_result()
@@ -84,12 +83,7 @@ class Worker:
 
     def add_cycle_to_worker(self, graph_node):
         # remain unpredicted labels are node whose neighbors are all unknown currently
-        # 1. add cycles which nodes are all known
         while graph_node.has_deleted_cycles():
-            # unknownTestNodes = self.unknown_url()
-            # # if unknownTestNodes == 0:
-            # #     break
-
             prevEdges = self.graph.number_of_edges()
             graph_node.add_deleted_cycles()
             currentEdges = self.graph.number_of_edges()
@@ -111,13 +105,6 @@ class Worker:
 
         return True
 
-    def unknown_url(self):
-        count = 0
-        for node in self.data['test']:
-            if self.graph.nodes[node]['predict_label'] == -1:
-                count += 1
-        return count
-
     def messagePassing(self, passType, iterationTimes):
         iterations = 0
         if passType == 'normal':
@@ -126,11 +113,9 @@ class Worker:
             unknownNodes = [e for e in list(self.graph.nodes()) if self.graph.nodes[e]['predict_label'] == -1]
 
         while True:
-
             message = Message(self.graph, self.edgePotentialType, self.similarity)
             message.set_thresholds(self.threshold1, self.threshold2)
             message.set_message_type(passType)
-            # TODO all change receive message, only hidden nodes receive msg
             outputs = [message.receive_message(node) for node in unknownNodes]
             message.update_graph_nodes_message(outputs)
             current_converged_nodes = message.count_converged_nodes(unknownNodes)
@@ -146,6 +131,3 @@ class Worker:
                 break
 
             iterations += 1
-
-        # accuracy, recall, precision, f1 = graph_node.calculate_performance()
-        # return {'accuracy': accuracy, 'recall': recall, 'precision': precision, 'f1': f1}
